619e285559c1abf67fd5e56142143657bdde7dac
Alexander Govorkov <37542986+agcentory240@users.noreply.github.com>
2022-03-22 21:59:24 +0300

    info update

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-## Compile for Linux
-env GOOS=linux go build
+# NWICODE v2 installation steps
 
-## Compile for Mac
-env GOOS=darmin go build
+Step-by-step instructions for installing Nwicode v2 CMS on hosting running Ubuntu 18 and higher.
 
-## Compile for Windows
-env GOOS=windows GOARCH=amd64 go build
+### 1. Install "git"
+    sudo apt-get update
+	sudo apt-get -y install git
+
+### 2. Clone installer from our repository
+    git clone https://github.com/nwicode/cms_install.git
+
+### 3. Install docker and  docker-compose
+    cd cms_install
+    chmod +x preinstall.sh
--- preinstall.sh
+++ preinstall.sh
@@ -3,7 +3,7 @@
 
 sudo apt-get update
 
-sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get -y remove docker docker-engine docker.io containerd runc
 
 sudo apt-get install \
     ca-certificates \
@@ -19,7 +19,7 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
